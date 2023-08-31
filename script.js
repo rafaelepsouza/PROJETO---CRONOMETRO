5a6923f9a659a65708b651b4d125ef5d739649b4
rafaelepsouza <rafaelexpedito.011@gmail.com>
2023-08-31 19:41:22 -0300

    Cronometro

---
+++ script.js
@@ -0,0 +1,14 @@
+let h1 = document.querySelector("h1")
+let number = 0
+let cron
+
+function start(){
+    cron = setInterval(() => {
+        number++
+        h1.innerHTML = number
+    }, 100)
+}
+
+function stop(){
+    clearInterval(cron)
+}
